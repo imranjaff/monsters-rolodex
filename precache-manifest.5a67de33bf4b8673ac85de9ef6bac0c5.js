2db0efc24a4a94ad1cbcd0910f8d5c57a4177823
Imran Jafri <imran.jafri@aptos.com>
2020-01-10 12:05:57 +0000

    Updates

---
+++ precache-manifest.5a67de33bf4b8673ac85de9ef6bac0c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e18c33972a8634eca3fe104d1db133",
+    "url": "/imranjaff/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "837b04c5ad1b6adf6453",
+    "url": "/imranjaff/monsters-rolodex/static/css/main.b4c5562c.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/imranjaff/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/imranjaff/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "837b04c5ad1b6adf6453",
+    "url": "/imranjaff/monsters-rolodex/static/js/main.d03f7d08.chunk.js"
+  },
+  {
+    "revision": "b2d72a93dadd9149297b",
+    "url": "/imranjaff/monsters-rolodex/static/js/runtime-main.6e484277.js"
+  }
+]);
